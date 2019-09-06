33ecfce4223a2e444a7a342d2324c4c2a7244592
Sunil Joshi <sunil.joshi@xyleminc.com>
2019-09-06 10:44:07 -0400

    added scheduler cron configuration

--- config-client/src/main/java/com/arcsq/poc/configclient/ConfigClientController.java
+++ config-client/src/main/java/com/arcsq/poc/configclient/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.arcsq.poc.configclient;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class ConfigClientController {
     @GetMapping("name")
     public String getNameOfApp() {
         return "Name of the app: " + name;
+    }
+
+    @Scheduled(cron = "${delay.ms}")
+    public void clearCache () {
+        System.out.println("Cache Cleared...");
     }
 
 }
